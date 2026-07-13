bde62b6e3e508382e3ff82bc449f001b7d3dee40
Hifihornet <alex.ljungbergs@icloud.com>
2026-07-13 20:44:48 +0200

    Initial Commit

---
+++ UppdragHund/UppdragHundApp.swift
@@ -0,0 +1,17 @@
+//
+//  UppdragHundApp.swift
+//  UppdragHund
+//
+//  Created by Alex  on 2026-07-13.
+//
+
+import SwiftUI
+
+@main
+struct UppdragHundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UppdragHundTests/UppdragHundTests.swift
@@ -0,0 +1,19 @@
+//
+//  UppdragHundTests.swift
+//  UppdragHundTests
+//
+//  Created by Alex  on 2026-07-13.
+//
+
+import Testing
+@testable import UppdragHund
+
+struct UppdragHundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
